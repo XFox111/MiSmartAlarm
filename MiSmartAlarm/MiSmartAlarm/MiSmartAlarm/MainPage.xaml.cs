62001a7401271587d42617970ee12335df81ea76
Michael Gordeev <michael.xfox@outlook.com>
2019-01-26 00:52:37 +0300

    Project init

---
+++ MiSmartAlarm/MiSmartAlarm/MiSmartAlarm/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MiSmartAlarm
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        
+        private void SettingsButton_Activated(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AboutButton_Activated(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new AboutPage());
+        }
+    }
+}
