7048a5ce3af7c9596cfe59bdf47280597b4eba44
Michael Gordeev <michael.xfox@outlook.com>
2019-01-29 12:18:47 +0300

    [Stage 2] Restoring changes

---
+++ MiSmartAlarm/MiSmartAlarm/MiSmartAlarm/AlarmPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MiSmartAlarm
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AlarmPage : ContentPage
+    {
+        public AlarmPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Activated(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
